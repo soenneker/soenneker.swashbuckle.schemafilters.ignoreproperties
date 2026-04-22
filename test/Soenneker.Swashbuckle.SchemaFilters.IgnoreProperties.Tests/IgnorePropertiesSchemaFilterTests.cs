b4a106ab5ab79e38f4e07ce9e0cb13ff124d6b35
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:44 -0500

    TUnit conversion

--- test/Soenneker.Swashbuckle.SchemaFilters.IgnoreProperties.Tests/IgnorePropertiesSchemaFilterTests.cs
+++ test/Soenneker.Swashbuckle.SchemaFilters.IgnoreProperties.Tests/IgnorePropertiesSchemaFilterTests.cs
@@ -1,16 +1,15 @@
-﻿using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Swashbuckle.SchemaFilters.IgnoreProperties.Tests;
 
-[Collection("Collection")]
-public class IgnorePropertiesSchemaFilterTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public class IgnorePropertiesSchemaFilterTests : HostedUnitTest
 {
-    public IgnorePropertiesSchemaFilterTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public IgnorePropertiesSchemaFilterTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
     }
